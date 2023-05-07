4527e8fe5d535ae2c5e23f2cadddb71916b5ef39
VuKiet <justinbk1412@gmail.com>
2023-05-07 21:46:47 +0700

    Update Booking.java

--- hotel-management-oop/src/BaseConstructor/Booking.java
+++ hotel-management-oop/src/BaseConstructor/Booking.java
@@ -16,7 +16,6 @@ public class Booking {
     private String typeTime;
     private int time;
     private String createAt;
-    private int quantity;
     private String paymentMethod;
     private Room[] listRoom;
 
@@ -37,7 +36,7 @@ public class Booking {
 
     }
 
-    public Booking(String bookingId, String customerName, String employeeName, double totalPrice, String typeTime, int time, String createAt, int quantity, String paymentMethod, Room[] listRoom) {
+    public Booking(String bookingId, String customerName, String employeeName, double totalPrice, String typeTime, int time, String createAt, String paymentMethod, Room[] listRoom) {
         this.bookingId = bookingId;
         this.customerName = customerName;
         this.employeeName = employeeName;
@@ -45,7 +44,6 @@ public class Booking {
         this.typeTime = typeTime;
         this.time = time;
         this.createAt = createAt;
-        this.quantity = quantity;
         this.paymentMethod = paymentMethod;
         this.listRoom = listRoom;
     }
@@ -106,13 +104,6 @@ public class Booking {
         this.createAt = createAt;
     }
 
-    public int getQuantity() {
-        return quantity;
-    }
-
-    public void setQuantity(int quantity) {
-        this.quantity = quantity;
-    }
 
     public String getPaymentMethod() {
         return paymentMethod;
@@ -174,10 +165,9 @@ public class Booking {
                     System.out.println("Ban chon phong VIP");
                     listRoomVIP.readListRoomVIP();
                     listRoomVIP.display();
-                    int existingRoom = listRoomVIP.countRoomVIP();
-                    listRoom = new Room[existingRoom];
+                    int existingRoomVIP = listRoomVIP.countRoomVIP();
 
-                    for(int i = 0; i< existingRoom;){
+                    for (int i = 0; i < existingRoomVIP; ) {
                         RoomVIP[] roomVIPs = listRoomVIP.getListRoom();
                         String roomVIPId;
                         do {
@@ -186,21 +176,130 @@ public class Booking {
                             String s = "RV[0-9]{3}";
                             Pattern pattern = Pattern.compile(s);
                             matcher = pattern.matcher(roomVIPId);
-                        }while (!matcher.find());
+                        } while (!matcher.find());
 
-                        Boolean check = false;
-                        for(int j = 0; j < roomVIPs.length; j++){
-                            String key = roomVIPs[j].getRoomVIPId();
-                            if(key.contentEquals(roomVIPId)){
-                                listRoom[i] = roomVIPs[j];
+                        boolean check = false;
 
+                        //Tim phong trong database
+                        for (RoomVIP roomVIP : roomVIPs) {
+                            String key = roomVIP.getRoomVIPId();
+                            if (key.contentEquals(roomVIPId)) {
+                                int selectTypeTime;
+                                String selectTypeTimeTemp;
+                                Matcher matcherTypeTime;
+
+                                do {
+                                    System.out.println("+------------------------------+");
+                                    System.out.println("|        Chon hinh thuc        |");
+                                    System.out.println("| ------------=====------------|");
+                                    System.out.println("| 1. Theo gio                  |");
+                                    System.out.println("| 2. Theo dem                  |");
+                                    System.out.println("| 3. Theo ngay                 |");
+                                    System.out.println("| 0. Tro lai                   |");
+                                    System.out.println("+------------------------------+");
+
+                                    do {
+                                        System.out.print("Nhap lua chon: ");
+                                        selectTypeTimeTemp = sc.nextLine();
+                                        String s = "^[0-9]{1}";
+                                        Pattern pattern = Pattern.compile(s);
+                                        matcherTypeTime = pattern.matcher(selectTypeTimeTemp);
+                                    } while (!matcherTypeTime.find());
+                                    selectTypeTime = Integer.parseInt(selectTypeTimeTemp);
+
+                                    switch (selectTypeTime) {
+                                        case 1:
+                                            System.out.println("Ban chon theo gio");
+                                            setTypeTime("theo gio");
+                                            System.out.print("Nhap so gio: ");
+                                            setTime(Integer.parseInt(sc.nextLine()));
+                                            setTotalPrice(getTime() * Double.parseDouble(roomVIP.getPricePerHour()));
+                                            break;
+                                        case 2:
+                                            System.out.println("Ban chon theo dem");
+                                            setTypeTime("theo dem");
+                                            setTime(1);
+                                            setTotalPrice(getTime() * Double.parseDouble(roomVIP.getPricePerNight()));
+                                            break;
+                                        case 3:
+                                            System.out.println("Ban chon theo ngay");
+                                            setTypeTime("theo ngay");
+                                            System.out.print("Nhap so ngay: ");
+                                            setTime(Integer.parseInt(sc.nextLine()));
+                                            setTotalPrice(getTime() * Double.parseDouble(roomVIP.getPricePerDay()));
+                                            break;
+                                        case 0:
+                                            System.out.println("Tro lai");
+                                            break;
+                                        default:
+                                            System.out.println("Loi lua chon! Vui long nhap lai");
+                                            break;
+                                    }
+
+                                } while (selectTypeTime != 0);
+
+                                check = true;
                             }
                         }
+
+                        if (!check)
+                            System.out.printf("\u001B[41m| Khong tim thay ma phong %s |\u001B[0m \n", roomVIPId);
+                        else
+                            i++;
                     }
+
+                    do {
+                        System.out.print("Nhap ngay dat phong: ");
+                        setCreateAt(sc.nextLine());
+                        String s = "^[0-9]{2}/[0-9]{2}/[0-9]{4}$";
+                        Pattern pattern = Pattern.compile(s);
+                        matcher = pattern.matcher(getCreateAt());
+                    } while (!matcher.find());
+
+                    int selectPaymentMethod;
+                    String selectPaymentMethodTemp;
+                    Matcher matcherPaymentMethod;
+//                    do {
+//                        System.out.println("+-------------------------------------------+");
+//                        System.out.println("|        Chon phuong thuc thanh toan        |");
+//                        System.out.println("| ------------------=====-------------------|");
+//                        System.out.println("| 1. Theo gio                               |");
+//                        System.out.println("| 2. Theo dem                               |");
+//                        System.out.println("| 3. Theo ngay                              |");
+//                        System.out.println("| 0. Tro lai                                |");
+//                        System.out.println("+-------------------------------------------+");
+//                    }
+
+
+// ket thuc case 1 --------------------------------------------------------------------------------------
+
+                case 2:
+                    System.out.println("Ban chon phong VIP");
+                    break;
+
+                case 0:
+                    System.out.println("Tro ra");
+                    break;
+                default:
+                    System.out.println("Loi lua chon! Vui long chon lai");
+                    break;
             }
+
 
         } while (select != 0);
 
+    }
+//------------Ket thuc input----------------------------------------------------------------------------------
+
+    public void getLineFromFile(String line){
+        String[] str = line.split(";");
+        setBookingId(str[0]);
+        setCustomerName(str[1]);
+        setEmployeeName(str[2]);
+        setTypeTime(str[3]);
+        setTime(Integer.parseInt(str[4]));
+        setCreateAt(str[5]);
+        setPaymentMethod(str[6]);
 
     }
 
