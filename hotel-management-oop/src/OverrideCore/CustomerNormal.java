ebc17eee7e70d82595ddd29b7aadf3286eb0789e
VuKiet <justinbk1412@gmail.com>
2023-05-08 20:39:42 +0700

    Revert "Merge branch 'dev' into feature/handleBooking"
    This reverts commit 12f6efc7b0ab14cc4199c606c3da60b582c580c3, reversing
    changes made to 0fd01888790415ba98964993bc9b9188217e3ed8.

--- hotel-management-oop/src/OverrideCore/CustomerNormal.java
+++ hotel-management-oop/src/OverrideCore/CustomerNormal.java
@@ -1,11 +1,4 @@
 package OverrideCore;
 
-import AbstractCore.TypeCustomer;
-import BaseConstructor.Customer;
-
-public class CustomerNormal extends Customer implements TypeCustomer {
-    @Override
-    public void typeCustomer(){
-        super.setTypeCustomer("Normal");
-    }
+public class CustomerNormal {
 }
